1a0600c227970db4d5b23601f1758be45853a784
SANSA HAMDI <sansa.hamdi@gmail.com>
2020-11-17 20:57:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './Style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+state = {
+  todo:[
+    {text:'learn react props', id:0, isComplete:false},
+    {text:'Learn react state', id:1, isComplete:false},
+    {text:'Learn react hooks', id:2, isComplete:false},
+    {text:'Learn react rooter', id:3, isComplete:false},
+  ],
+  textInput:''
 }
 
-export default App;
+  render() {
+
+const handleComplete=(index)=>{
+  this.setState({todo : this.state.todo.map(el=>{
+    return (el.id===index ? {...el, isComplete: !el.isComplete} : el)
+  })})
+}
+
+const handleDelete=(index)=>{
+  this.setState({todo : this.state.todo.filter(el=>{
+    return el.id !== index
+  })})
+}
+
+
+const handleSubmit=(e)=>{
+  this.setState({textInput: e.target.value})
+}
+
+
+const handleAdd=(e)=>{
+  e.preventDefault();
+  if(this.state.textInput.trim()){
+  const test = {text: this.state.textInput, id:Date.now()}
+  this.setState({todo: [...this.state.todo, test]})
+  this.state.textInput=''
+  } else{
+    alert('could you please put your to do again')
+  }
+}
+
+    return (
+      <div className='App container'>
+        <form className='form' onSubmit={this.state.textInput}>
+        <h1 className='container'>To Do List</h1>
+          <input value={this.state.textInput} onChange={(e)=>handleSubmit(e)} type='text' />
+            <button onClick={handleAdd} className='btn-primary'>Add</button>
+        </form>
+        {this.state.todo.map(el=> 
+          <div className='todo mt-3' key={el.id} >
+          <div className='mt-3 ml-3'>
+            <p className={el.isComplete ? 'done' : ''}>{el.text}</p>
+          </div>
+          <div className='mr-2'>
+            <button onClick={()=>handleDelete(el.id)} className='btn-danger'>Delete</button>
+            <button onClick={()=>handleComplete(el.id)} className='btn-secondary'> {!el.isComplete ? 'Complete' : 'Undo'} </button>
+          </div>
+        </div>
+          )}
+        
+        
+      </div>
+    )
+  }
+}
+
+export default App
